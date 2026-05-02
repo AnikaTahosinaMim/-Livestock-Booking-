81de2c4b30a878f914b6bf673f937879e2bdfc9d
anika557 <ummehabiba19758@gmail.com>
2026-05-02 17:46:52 +0600

    domain

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -19,12 +19,5 @@ export const auth = betterAuth({
             clientSecret: process.env.GOOGLE_SECRATE, 
         }, 
     },
-     baseURL: {
-    allowedHosts: [
-      "livestock-booking-5dpp.vercel.app", 
-      "livestock-booking-xuig.vercel.app", 
-      "*.vercel.app"
-    ],
-    protocol: "https",
-  },
+    
 });
